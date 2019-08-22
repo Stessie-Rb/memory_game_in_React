1334b113e0b1da1c29048f73261780f20ccd7328
Stessie <enjoyyl@outlook.fr>
2019-08-22 16:25:29 +0200

    tests step

---
+++ src/setupTests.js
@@ -0,0 +1,18 @@
+import Adapter from 'enzyme-adapter-react-16'
+import chai from 'chai'
+import { configure as configureEnzyme } from 'enzyme'
+import createChaiEnzyme from 'chai-enzyme'
+import createChaiJestDiff from 'chai-jest-diff'
+import chaiJestSnapshot from 'chai-jest-snapshot'
+import enzymeToJson from 'enzyme-to-json/serializer'
+import dirtyChai from 'dirty-chai'
+import sinonChai from 'sinon-chai'
+chai
+  .use(dirtyChai)
+  .use(createChaiJestDiff())
+  .use(chaiJestSnapshot)
+  .use(createChaiEnzyme())
+  .use(sinonChai)
+
+expect.addSnapshotSerializer(enzymeToJson)
+configureEnzyme({ adapter: new Adapter() })
